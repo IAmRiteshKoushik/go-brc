a011410ebe55e781bac1a60caebd0374564833fa
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-03-17 11:37:43 +0530

    custom byte to int converter

---
+++ custom_int_converter.go
@@ -0,0 +1,22 @@
+package main
+
+// All the bytes supplied here will be either +, - or 0...9
+func bytesToInt(byteArray []byte) int {
+	var result int
+	negative := false
+
+	for _, b := range byteArray {
+		if b == 45 { // 45 = "-" signal, as per ASCII conventions
+			negative = true
+			continue
+		}
+		// For each new number, move the old number one digit to left; byte shifting
+		result = result*10 + int(b-48) // 48 = '0', 49 = '1', ...
+	}
+
+	// handle negative numbers
+	if negative {
+		return -result
+	}
+	return result
+}
