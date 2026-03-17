93ab5a1f652466a52082339957c7ac67670e7153
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-03-17 12:34:17 +0530

    fix buffer issues and send leftovers to future iteration

---
+++ attempt_9_fixed_buffer_issues.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+const bufferSize = 512 * 512
+
+func attemptNine() {
+	channel := make(chan []byte, 10)
+	go consumer(channel)
+
+	file, err := os.Open("measurements.txt")
+	if err != nil {
+	}
+
+	// Out of loop's scope
+	readBuffer := make([]byte, bufferSize)
+	leftoverBuffer := make([]byte, 1024) // a little bit extra
+	leftoverSize := 0
+
+	// Read the file in chunks and send it via a channel to consumer
+	for {
+		_, err := file.Read(readBuffer)
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		// Find the last newline character "\n" (byte = 10). Loop from end to
+		// first and then find it
+		m := 0
+		for i := bufferSize - 1; i >= 0; i-- {
+			if readBuffer[i] == 10 {
+				m = i
+				break
+			}
+		}
+
+		// Byte buffer is created base current valid size and previous leftover
+		data := make([]byte, m+leftoverSize)
+
+		// data buffer is populated based on previous leftover and the next complete
+		// byte set
+		copy(data, leftoverBuffer[:leftoverSize])
+		copy(data[leftoverSize:], readBuffer[:m])
+
+		// Leftover buffer for the next iteration with incomplete byte set
+		copy(leftoverBuffer, readBuffer[m+1:bufferSize])
+		leftoverSize = bufferSize - m - 1
+
+		channel <- data
+	}
+}
