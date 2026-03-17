79efea1d9d1d7d76f198c879103519aeb1576cb9
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-03-17 12:15:48 +0530

    move start time variable inside each switch case

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"sort"
 	"time"
 )
 
@@ -17,55 +16,52 @@ type StationData struct {
 
 func main() {
 	flag := os.Args[1]
-	start := time.Now()
 	switch flag {
 	case "one":
+		start := time.Now()
 		attemptOne() // read and compute
 		fmt.Printf("%0.6f\n", time.Since(start).Seconds())
 	case "two":
+		start := time.Now()
 		attemptTwo() // read-only
 		fmt.Printf("%0.6f\n", time.Since(start).Seconds())
 	case "three":
 		for range 8 {
+			start := time.Now()
 			attemptThree(1024 * 1024) // read-only
 			fmt.Printf("%0.6f\n", time.Since(start).Seconds())
 		}
 	case "four":
+		start := time.Now()
 		attemptFour() // read-only, using a reader.ReadBytes instead of a scanner
 		fmt.Printf("%0.6f\n", time.Since(start).Seconds())
 	case "five":
+		start := time.Now()
 		attemptFive() // read-only, using a reader.ReadLine instead of a scanner
 		fmt.Printf("%0.6f\n", time.Since(start).Seconds())
 	case "six":
 		for range 4 {
+			start := time.Now()
 			attemptSix() // read-only, using a file.Read
 			fmt.Printf("%0.6f\n", time.Since(start).Seconds())
 		}
 	case "seven":
 		for range 4 {
+			start := time.Now()
 			attemptSeven(512 * 512)
 			fmt.Printf("%0.6f\n", time.Since(start).Seconds())
 		}
+	case "eight":
+		for range 4 {
+			start := time.Now()
+			attemptEight(512 * 512)
+			fmt.Printf("%0.6f\n", time.Since(start).Seconds())
+		}
+	case "nine":
+		for range 4 {
+			start := time.Now()
+			attemptNine()
+			fmt.Printf("%0.6f\n", time.Since(start).Seconds())
+		}
 	}
-}
-
-func printResult(data map[string]*StationData) {
-	result := make(map[string]*StationData, len(data))
-	keys := make([]string, 0, len(data))
-	for _, v := range data {
-		keys = append(keys, v.Name)
-		result[v.Name] = v
-	}
-	// We are sorting things alphabetically and then printing, this adds a bit of
-	// additional overhead especially when processing 40k unique names
-	sort.Strings(keys)
-
-	print("{\n")
-	for _, k := range keys {
-		v := result[k]
-
-		// Printing <station-name>=min/avg/max
-		fmt.Printf("%s=%.1f/%.1f/%.1f\n", k, v.Min, v.Sum/float64(v.Count), v.Max)
-	}
-	print("}\n")
 }
