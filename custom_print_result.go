2e4c6e44b2e5dbd0166c48e9074cf3715c6e561f
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-03-17 12:16:07 +0530

    move printResult into a separate file to keep the main.go clean for growing switchcase

---
+++ custom_print_result.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func printResult(data map[string]*StationData) {
+	result := make(map[string]*StationData, len(data))
+	keys := make([]string, 0, len(data))
+	for _, v := range data {
+		keys = append(keys, v.Name)
+		result[v.Name] = v
+	}
+	// We are sorting things alphabetically and then printing, this adds a bit of
+	// additional overhead especially when processing 40k unique names
+	sort.Strings(keys)
+
+	print("{\n")
+	for _, k := range keys {
+		v := result[k]
+
+		// Printing <station-name>=min/avg/max
+		fmt.Printf("%s=%.1f/%.1f/%.1f\n", k, v.Min, v.Sum/float64(v.Count), v.Max)
+	}
+	print("}\n")
+}
