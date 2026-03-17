fe62ead3e571a3d197055888892cbe713e5f7e5b
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-03-17 11:38:30 +0530

    convert main into a barrel file

--- main.go
+++ main.go
@@ -1,59 +1,71 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"time"
 )
 
-func run_bytes() {
-	/*
-	 * Reading through the entire file using an internal buffer
-	 * */
-	file, err := os.Open("measurements.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	// String conversion is slower due to additional overhead as it involved
-	// allocation of memory
-	for scanner.Scan() {
-		// Use an internal buffer returning the same object so there is no additional
-		// allocation
-		scanner.Bytes()
-	}
-
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
+type StationData struct {
+	Name  string
+	Min   float64
+	Max   float64
+	Sum   float64
+	Count int
 }
 
-func run_strings() {
-	/*
-	 * Reading through the entire file line by line in the form of strings
-	 * */
-
-	file, err := os.Open("measurements.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		scanner.Text()
-	}
-}
-
-// Driver program
 func main() {
-	started := time.Now()
-	run_bytes()
-	fmt.Printf("Time taken to read 13GB file in byte-buffer: %.6f\n", time.Since(started).Seconds())
-	started = time.Now()
-	run_strings()
-	fmt.Printf("Time taken to read 13GB file in strings: %.6f", time.Since(started).Seconds())
+	flag := os.Args[1]
+	start := time.Now()
+	switch flag {
+	case "one":
+		attemptOne() // read and compute
+		fmt.Printf("%0.6f\n", time.Since(start).Seconds())
+	case "two":
+		attemptTwo() // read-only
+		fmt.Printf("%0.6f\n", time.Since(start).Seconds())
+	case "three":
+		for range 8 {
+			attemptThree(1024 * 1024) // read-only
+			fmt.Printf("%0.6f\n", time.Since(start).Seconds())
+		}
+	case "four":
+		attemptFour() // read-only, using a reader.ReadBytes instead of a scanner
+		fmt.Printf("%0.6f\n", time.Since(start).Seconds())
+	case "five":
+		attemptFive() // read-only, using a reader.ReadLine instead of a scanner
+		fmt.Printf("%0.6f\n", time.Since(start).Seconds())
+	case "six":
+		for range 4 {
+			attemptSix() // read-only, using a file.Read
+			fmt.Printf("%0.6f\n", time.Since(start).Seconds())
+		}
+	case "seven":
+		for range 4 {
+			attemptSeven(512 * 512)
+			fmt.Printf("%0.6f\n", time.Since(start).Seconds())
+		}
+	}
+}
+
+func printResult(data map[string]*StationData) {
+	result := make(map[string]*StationData, len(data))
+	keys := make([]string, 0, len(data))
+	for _, v := range data {
+		keys = append(keys, v.Name)
+		result[v.Name] = v
+	}
+	// We are sorting things alphabetically and then printing, this adds a bit of
+	// additional overhead especially when processing 40k unique names
+	sort.Strings(keys)
+
+	print("{\n")
+	for _, k := range keys {
+		v := result[k]
+
+		// Printing <station-name>=min/avg/max
+		fmt.Printf("%s=%.1f/%.1f/%.1f\n", k, v.Min, v.Sum/float64(v.Count), v.Max)
+	}
+	print("}\n")
 }
