dfb6ee41e758e32b42867af08bf180574423f9db
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-03-17 12:16:18 +0530

    fix race condition in sync issue

--- attempt_8_better_sync.go
+++ attempt_8_better_sync.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"io"
+	"os"
+)
+
+func attemptEight(BUFFER_SIZE int) {
+	channel := make(chan []byte, 10)
+	go consumer(channel)
+
+	file, err := os.Open("measurements.txt")
+	if err != nil {
+	}
+
+	buffer := make([]byte, BUFFER_SIZE)
+
+	for {
+		_, err := file.Read(buffer)
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		// Instead of sending the buffer directly, copy a part of the buffer and
+		// send it to the consumer. Reasons for doing this is explained in the notes
+		data := make([]byte, BUFFER_SIZE)
+		copy(data, buffer[:BUFFER_SIZE])
+		channel <- data
+	}
+}
